f224c85f71a5d5d8f4ace8643f2c874ea3087ad8
vinay chandran <vinay.sarvodaya1991@gmail.com>
2023-07-27 18:25:39 +0530

    string methods - length, slice

---
+++ stringMethods.js
@@ -0,0 +1,13 @@
+// string methods in javascript
+
+// 1. length
+let txt = "vinay"
+// console.log(txt.length)
+//ans-5
+// The length property returns the length of a string:
+
+// 2.slice
+let name = "vinaychandran"
+// console.log(name.slice(5, 13))
+// ans - chandran
+// slice() extracts a part of a string and returns the extracted part in a new string.

01f804fcb75c52d67404a019ae6f843728fa0692
vinay chandran <vinay.sarvodaya1991@gmail.com>
2023-07-27 18:44:09 +0530

    string methods - replace,toUpperCase,toLowerCase

--- stringMethods.js
+++ stringMethods.js
@@ -6,8 +6,27 @@ let txt = "vinay"
 //ans-5
 // The length property returns the length of a string:
 
-// 2.slice
+// 2.slice()
 let name = "vinaychandran"
 // console.log(name.slice(5, 13))
 // ans - chandran
 // slice() extracts a part of a string and returns the extracted part in a new string.
+
+// 3. replace()
+let character = "shankar dayal"
+// console.log(character.replace("dayal", "mahadevan"))
+// ans- "shankar mahadevan"
+// The replace() method replaces a specified value with another value in a string.
+// The replace() method does not change the string it is called on.
+// The replace() method returns a new string.
+// The replace() method replaces only the first match
+
+// 4. toUpperCase()
+let names = "vinay"
+// console.log(names.toUpperCase(names))
+// ans- VINAY
+
+// 5. toLowerCase()
+let nameLowerCase = "VINAY"
+console.log(nameLowerCase.toLowerCase())
+// ans - vinay
